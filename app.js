b6d9e489f3d127799175bbc2c7b4c7eb37a88547
Steven <s.millot@neolynk.fr>
2019-09-17 01:03:54 +0200

    Send json message

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('//', (req, res) => {
+  res.json({ message: 'Hello World' })
 })
 
-app.listen(3000, function () {
+
+
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
