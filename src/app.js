011063093b63030e0c346faf3ca646918cf0d13f
Steven Millot <steven.millot@gmail.com>
2019-10-09 12:55:27 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 // On doit pouvoir avec ce module démarrer un serveur HTTP avec les paramètres que l'on souhaite.
 const express = require('express')
 
-module.exports = {
+module.exports = {  
   servStart: (hostname, port) => {
 
+    // Création de l'application
     const app = express()
+    
+
+    // Configuration de l'application
+    let users = require('../assets/users.json')
+
+    /**
+     * Configure la route '/'
+     * pour envoyer le message json
+     * log la date d'appel
+     */
+    app.get('/',
+      (req, res) => {
+        req.header.autorization ==> 
+        res.json({ message: 'Hello World !' })
+        console.log('Time of \'/\' route call :', Date.now())
+      })
+
+    /**
+     * Configure la route '/user/me'
+     * log la date d'appel
+     */
+    app.get('/user/me', (req, res) => {
+      res.json({ users })
+      console.log('Time of \'/user/me\' route call :', Date.now())
+    })
 
     // console.log('export app -> ', app)
 
