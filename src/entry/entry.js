d8b5f15ec40f747374acbab384aa4965c6ce1caa
Steven Millot <steven.millot@gmail.com>
2019-10-09 12:55:37 +0200

    Update entry.js

--- src/entry/entry.js
+++ src/entry/entry.js
@@ -4,28 +4,6 @@
 const servCreator = require('../app')
 const { hostname, port } = require('../config/appConf')
 
-const app = servCreator.servStart(hostname, port)
 
-let users = require('../assets/users.json')
 
-/**
- * Configure la route '/'
- * pour envoyer le message json
- * log la date d'appel
- */
-app.get('/',
-  (req, res) => {
-    res.json({ message: 'Hello World !' })
-    console.log('Time of \'/\' route call :', Date.now())
-  })
-
-/**
- * Configure la route '/user/me'
- * log la date d'appel
- */
-app.get('/user/me', (req, res) => {
-  res.json({ users })
-  console.log('Time of \'/user/me\' route call :', Date.now())
-})
-
-// servCreator.servStart(hostname, port)
+servCreator.servStart(hostname, port)
